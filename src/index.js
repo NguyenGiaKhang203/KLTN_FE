5725cca20c7ff4cc49dcbf1a4c08ac8cabf835cf
VÂNHI <Kangyenzt203@gmail.com>
2025-03-16 21:52:56 +0700

    SignInPage

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const queryClient = new QueryClient()
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient();
 root.render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </QueryClientProvider>
 );
 
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,26 +1,130 @@
-import axios from "axios"
+import axios from "axios";
 
-export const axiosJWT = axios.create()
+export const axiosJWT = axios.create();
 
 export const loginUser = async (data) => {
-      const res = await axios.post(`${process.env.REACT_APP_API_URL}/user/sign-in`, data);
-      return res.data;
-} 
+  const res = await axios.post(
+    `${process.env.REACT_APP_API_URL}/user/sign-in`,
+    data
+  );
+  return res.data;
+};
 
 export const signupUser = async (data) => {
-        const res = await axios.post(`${process.env.REACT_APP_API_URL}/user/sign-up`, data); 
-        return res.data; 
-}
+  const res = await axios.post(
+    `${process.env.REACT_APP_API_URL}/user/sign-up`,
+    data
+  );
+  return res.data;
+};
 
 export const sendOtp = async (data) => {
-    try {
-        const res = await axios.post(`${process.env.REACT_APP_API_URL}/user/send-otp`, data);
-        return res.data;
-    } catch (error) {
-        throw {
-            status: 'ERR',
-            message: error.response?.data?.message || 'Có lỗi xảy ra khi gửi OTP.'
-        }
-    }
-}
+  try {
+    const res = await axios.post(
+      `${process.env.REACT_APP_API_URL}/user/send-otp`,
+      data
+    );
+    return res.data;
+  } catch (error) {
+    throw {
+      status: "ERR",
+      message: error.response?.data?.message || "Có lỗi xảy ra khi gửi OTP.",
+    };
+  }
+};
 
+export const resetPassword = async (data) => {
+  const res = await axios.post(
+    `${process.env.REACT_APP_API_URL}/user/reset-password`,
+    data
+  );
+  return res.data;
+};
+
+export const resendOtp = async (data) => {
+  const res = await axios.post(
+    `${process.env.REACT_APP_API_URL}/user/resend-otp`,
+    data
+  );
+  return res.data;
+};
+
+export const getDetailsUser = async (id, access_token) => {
+  const res = await axiosJWT.get(
+    `${process.env.REACT_APP_API_URL}/user/get-details/${id}`,
+    {
+      headers: {
+        token: `Bearer ${access_token}`,
+      },
+    }
+  );
+  return res.data;
+};
+
+export const deleteUser = async (id, access_token, data) => {
+  const res = await axiosJWT.delete(
+    `${process.env.REACT_APP_API_URL}/user/delete-user/${id}`,
+    data,
+    {
+      headers: {
+        token: `Bearer ${access_token}`,
+      },
+    }
+  );
+  return res.data;
+};
+export const getAllUser = async (access_token) => {
+  const res = await axiosJWT.get(
+    `${process.env.REACT_APP_API_URL}/user/getAll`,
+    {
+      headers: {
+        token: `Bearer ${access_token}`,
+      },
+    }
+  );
+  return res.data;
+};
+export const refreshToken = async (refreshToken) => {
+  console.log("refreshToken", refreshToken);
+  const res = await axios.post(
+    `${process.env.REACT_APP_API_URL}/user/refresh-token`,
+    {},
+    {
+      headers: {
+        token: `Bearer ${refreshToken}`,
+      },
+    }
+  );
+  return res.data;
+};
+
+export const logoutUser = async () => {
+  const res = await axios.post(`${process.env.REACT_APP_API_URL}/user/log-out`);
+  return res.data;
+};
+
+export const updateUser = async (id, data, access_token) => {
+  const res = await axiosJWT.put(
+    `${process.env.REACT_APP_API_URL}/user/update-user/${id}`,
+    data,
+    {
+      headers: {
+        token: `Bearer ${access_token}`,
+      },
+    }
+  );
+  return res.data;
+};
+
+export const deleteManyUser = async (data, access_token) => {
+  const res = await axiosJWT.post(
+    `${process.env.REACT_APP_API_URL}/user/delete-many`,
+    data,
+    {
+      headers: {
+        token: `Bearer ${access_token}`,
+      },
+    }
+  );
+  return res.data;
+};
