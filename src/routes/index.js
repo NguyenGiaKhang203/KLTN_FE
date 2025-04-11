11922993c2db0912d630c661549e7bafe42ee256
Hoang Anh <hoanganhtt11@gmail.com>
2025-04-11 23:41:35 +0700

    Update

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,7 +14,13 @@ import AdminDashboard from "../pages/Admin/AdminDashboard/AdminDashboard";
 import SchedulePage from "../pages/Admin/SchedulePage/SchedulePage";
 import StudentPage from "../pages/Admin/StudentPage/StudentPage";
 import ContactPage from "../pages/ContactPages/ContactPage";
-
+import TeacherPage from "../pages/Admin/TeacherPage/TeacherPage";
+import ExamPage from "../pages/Admin/TeacherPage/TeacherPage";
+import PaymentManagement from "../pages/Admin/PaymentManagementPage/PaymentManagement";
+import ReportPage from "../pages/Admin/ReportPage/ReportPage";
+import AssessPage from "../pages/Admin/AssessPage/AssessPage";
+import AttendancePage from "../pages/Admin/AttendancePage/AttendancePage";
+import StudentschedulePage from "../pages/StudentschedulePage/StudentschedulePage"
 export const routes = [
   {
     path: "/",
@@ -83,6 +89,13 @@ export const routes = [
     isShowHeader: true,
     isShowFooter: true,
   },
+  {
+    path: "/schedule",
+    page: StudentschedulePage,
+    isShowHeader: true,
+    isShowFooter: false,
+  },
+
   // Admin Routes
   {
     path: "/system/admin",
@@ -108,6 +121,31 @@ export const routes = [
         path: "students",
         page: StudentPage,
       },
+      {
+        path: "teachers",
+        page: TeacherPage,
+      },
+      {
+        path: "exams",
+        page: ExamPage,
+      },
+      {
+        path: "payment",
+        page: PaymentManagement,
+      },
+      {
+        path: "assess",
+        page: AssessPage,
+      },
+      {
+        path: "report",
+        page: ReportPage,
+      },
+      {
+        path: "attendance",
+        page: AttendancePage,
+      },
+
     ],
   },
 ];
